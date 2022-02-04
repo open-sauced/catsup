a4e5be665ce85dd4ecd8c58822f593cb9f67b464
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2022-02-04 15:30:36 -0800

    watches for labels

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fetch = require("node-fetch")
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
@@ -6,22 +7,20 @@ module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
 
-  app.on("issues.reopened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
+  const GOOD_FIRST_REGEX = /^good\sfirst\sissue$/i
+
+  app.on("issues.labeled", async (context) => {
+    const labels = context.payload.issue.labels
+
+    if (!GOOD_FIRST_REGEX.test(context.payload.label.name)) return
+    console.log(context.payload.issue.html_url)
+
     // send message to discord
-    const message = {
-      content: "Thanks for opening this issue!",
-      username: "Probot",
-      avatar_url: "https://avatars2.githubusercontent.com/u/3300?s=460&v=4",
-    };
-    const channel = "general";
-    const webhook = DISCORD_WEBHOOK_URL;
+    const webhook = process.env.DISCORD_WEBHOOK_URL;
     const params = {
       username: "Test TestyFace",
       avatar_url: "https://res.cloudinary.com/practicaldev/image/fetch/s--aIRKIzVN--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/330216/51029220-5eff-400f-b8a8-02660bb8c9be.png",
-      content: "There is an opened issue"
+      content: "New good first issue: " + context.payload.issue.html_url
     }
 
     // send post request using fetch to webhook
