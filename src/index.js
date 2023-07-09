302bc297d2d37267cfd557f9cbe23e623d1169d1
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2023-07-08 18:50:12 -0700

    use `octokit` instead of `@octokit/app` and simplify code

--- src/index.js
+++ src/index.js
@@ -1,31 +1,45 @@
-import { setup, webhooks, verifySignature } from './octokit';
+import { App } from "octokit";
 
-const application = setup();
-webhooks(application);
+const GOOD_FIRST_REGEX = /^good\sfirst\sissue$/i;
 
-const dashboard = async (app) => {
-  const { data } = await app.octokit.request('GET /app');
+// instantiate app
+const app = new App({
+  appId: APP_ID,
+  privateKey: APP_PK,
+  oauth: {
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+  },
+  webhooks: {
+    secret: WEBHOOK_SECRET,
+  },
+});
 
-  return new Response(`
-<h1>GitHub App: ${data.name}</h1>
+app.log.warn("Yay, the app was loaded!");
 
-<p>Installation count: ${data.installations_count}</p>
+// listen to webhooks
+app.webhooks.on("issues.labeled", async ({ payload }) => {
+  const { name } = payload.label;
 
-<p>
-    <a href="https://github.com/apps/${data.slug}">
-        <img src="https://img.shields.io/static/v1?label=Install%20App:&message=${
-  data.slug
-}&color=orange&logo=github&style=for-the-badge" alt="Install ${data.name}"/>
-    </a>
-</p>
+  if (!GOOD_FIRST_REGEX.test(name)) return;
 
-<p>
-    <a href="https://github.com/0-vortex/open-sauced-catsup-app-test/#readme">source code</a>
-</p>
-`, {
-    headers: { 'content-type': 'text/html' },
+  // send message to discord
+  const webhook = DISCORD_URL;
+  const params = {
+    username: "GFI-Catsup [beta]",
+    avatar_url: "https://github.com/open-sauced/assets/blob/master/logo.png?raw=true",
+    content: `New good first issue: ${payload.issue.html_url}`,
+  };
+
+  // send post request using fetch to webhook
+  await fetch(webhook, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(params),
   });
-};
+});
 
 /**
  * Respond with hello worker text
@@ -33,27 +47,58 @@ const dashboard = async (app) => {
  */
 async function handleRequest(request) {
   // display installation dashboard
-  if (request.method === 'GET') {
+  if (request.method === "GET") {
     return dashboard(application);
   }
 
   // else verify webhook signature
   try {
-    await verifySignature(application, request);
+    await app.webhooks.verifyAndReceive({
+      id: request.headers.get("x-github-delivery"),
+      name: request.headers.get("x-github-event"),
+      signature: request.headers.get("x-hub-signature-256"),
+      payload: await request.text(),
+    });
 
     return new Response('{ "ok": true }', {
-      headers: { 'content-type': 'application/json' },
+      headers: { "content-type": "application/json" },
     });
   } catch (error) {
-    application.log.error(error);
+    app.log.error(error);
 
-    return new Response(`{ "error": "${error.message}" }`, {
+    return new Response(`{ 'error': '${error.message}' }`, {
       status: 500,
-      headers: { 'content-type': 'application/json' },
+      headers: { "content-type": "application/json" },
     });
   }
 }
 
-addEventListener('fetch', (event) => {
+// dashboard response
+const dashboard = async () => {
+  const { data } = await app.octokit.request("GET /app");
+
+  return new Response(
+    `
+<h1>GitHub App: ${data.name}</h1>
+
+<p>Installation count: ${data.installations_count}</p>
+
+<p>
+    <a href='https://github.com/apps/${data.slug}'>
+        <img src='https://img.shields.io/static/v1?label=Install%20App:&message=${data.slug}&color=orange&logo=github&style=for-the-badge' alt='Install ${data.name}'/>
+    </a>
+</p>
+
+<p>
+    <a href='https://github.com/0-vortex/open-sauced-catsup-app-test/#readme'>source code</a>
+</p>
+`,
+    {
+      headers: { "content-type": "text/html" },
+    }
+  );
+};
+
+addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
