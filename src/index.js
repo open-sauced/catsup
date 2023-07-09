6e0fb50896f241e0d905b49ba8ecbe7ae2b28f45
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2023-07-08 19:01:20 -0700

    style: eslint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { App } from "@octokit/app/dist-src";
+import { App } from '@octokit/app/dist-src';
 
 const GOOD_FIRST_REGEX = /^good\sfirst\sissue$/i;
 
@@ -15,10 +15,10 @@ const app = new App({
   },
 });
 
-app.log.warn("Yay, the app was loaded!");
+app.log.warn('Yay, the app was loaded!');
 
 // listen to webhooks
-app.webhooks.on("issues.labeled", async ({ payload }) => {
+app.webhooks.on('issues.labeled', async ({ payload }) => {
   const { name } = payload.label;
 
   if (!GOOD_FIRST_REGEX.test(name)) return;
@@ -26,16 +26,16 @@ app.webhooks.on("issues.labeled", async ({ payload }) => {
   // send message to discord
   const webhook = DISCORD_URL;
   const params = {
-    username: "GFI-Catsup [beta]",
-    avatar_url: "https://github.com/open-sauced/assets/blob/master/logo.png?raw=true",
+    username: 'GFI-Catsup [beta]',
+    avatar_url: 'https://github.com/open-sauced/assets/blob/master/logo.png?raw=true',
     content: `New good first issue: ${payload.issue.html_url}`,
   };
 
   // send post request using fetch to webhook
   await fetch(webhook, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(params),
   });
@@ -47,35 +47,35 @@ app.webhooks.on("issues.labeled", async ({ payload }) => {
  */
 async function handleRequest(request) {
   // display installation dashboard
-  if (request.method === "GET") {
+  if (request.method === 'GET') {
     return dashboard(application);
   }
 
   // else verify webhook signature
   try {
     await app.webhooks.verifyAndReceive({
-      id: request.headers.get("x-github-delivery"),
-      name: request.headers.get("x-github-event"),
-      signature: request.headers.get("x-hub-signature-256"),
+      id: request.headers.get('x-github-delivery'),
+      name: request.headers.get('x-github-event'),
+      signature: request.headers.get('x-hub-signature-256'),
       payload: await request.text(),
     });
 
     return new Response('{ "ok": true }', {
-      headers: { "content-type": "application/json" },
+      headers: { 'content-type': 'application/json' },
     });
   } catch (error) {
     app.log.error(error);
 
     return new Response(`{ 'error': '${error.message}' }`, {
       status: 500,
-      headers: { "content-type": "application/json" },
+      headers: { 'content-type': 'application/json' },
     });
   }
 }
 
 // dashboard response
 const dashboard = async () => {
-  const { data } = await app.octokit.request("GET /app");
+  const { data } = await app.octokit.request('GET /app');
 
   return new Response(
     `
@@ -94,11 +94,11 @@ const dashboard = async () => {
 </p>
 `,
     {
-      headers: { "content-type": "text/html" },
-    }
+      headers: { 'content-type': 'text/html' },
+    },
   );
 };
 
-addEventListener("fetch", (event) => {
+addEventListener('fetch', (event) => {
   event.respondWith(handleRequest(event.request));
 });
