2330d7f8f4f25e9f4cb11bfcebc4b80d27799809
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2023-07-10 21:54:03 -0700

    add some logging for debugging

--- netlify/edge-functions/github-webhooks.js
+++ netlify/edge-functions/github-webhooks.js
@@ -1,63 +1,68 @@
 // @ts-check
 
-import { App } from 'https://esm.sh/@octokit/app@13.1.8';
+import { App } from "https://esm.sh/@octokit/app@13.1.8";
 
 const GOOD_FIRST_REGEX = /^good\sfirst\sissue$/i;
 
 const requiredEnvironmentVariables = [
-  'GITHUB_APP_ID',
-  'GITHUB_APP_PRIVATE_KEY',
-  'GITHUB_APP_CLIENT_ID',
-  'GITHUB_APP_CLIENT_SECRET',
-  'GITHUB_APP_WEBHOOK_SECRET',
-  'DISCORD_WEBHOOKS_URL',
+  "GITHUB_APP_ID",
+  "GITHUB_APP_PRIVATE_KEY",
+  "GITHUB_APP_CLIENT_ID",
+  "GITHUB_APP_CLIENT_SECRET",
+  "GITHUB_APP_WEBHOOK_SECRET",
+  "DISCORD_WEBHOOKS_URL",
 ];
 
-const missingEnvironmentVariables = requiredEnvironmentVariables.filter(
-  (name) => !Deno.env.get(name),
-);
+const missingEnvironmentVariables = requiredEnvironmentVariables.filter((name) => !Deno.env.get(name));
 
 if (missingEnvironmentVariables.length) {
-  throw new Error(`Missing environment variables: ${missingEnvironmentVariables.join(', ')}`);
+  throw new Error(`Missing environment variables: ${missingEnvironmentVariables.join(", ")}`);
 }
 
 const app = new App({
-  appId: Number(Deno.env.get('GITHUB_APP_ID')),
-  privateKey: Deno.env.get('GITHUB_APP_PRIVATE_KEY'),
+  appId: Number(Deno.env.get("GITHUB_APP_ID")),
+  privateKey: Deno.env.get("GITHUB_APP_PRIVATE_KEY"),
   oauth: {
-    clientId: Deno.env.get('GITHUB_APP_CLIENT_ID'),
-    clientSecret: Deno.env.get('GITHUB_APP_CLIENT_SECRET'),
+    clientId: Deno.env.get("GITHUB_APP_CLIENT_ID"),
+    clientSecret: Deno.env.get("GITHUB_APP_CLIENT_SECRET"),
   },
   webhooks: {
-    secret: Deno.env.get('GITHUB_APP_WEBHOOK_SECRET'),
+    secret: Deno.env.get("GITHUB_APP_WEBHOOK_SECRET"),
   },
+  log: console,
 });
 
-app.webhooks.on('issues.labeled', async (context) => {
+app.log.info("App loaded");
+
+app.webhooks.onAny(async ({ name, payload }) => {
+  app.log.info(`Event received: ${name}.${payload.action}`);
+});
+
+app.webhooks.on("issues.labeled", async (context) => {
   const { name } = context.payload.label;
 
   if (!GOOD_FIRST_REGEX.test(name)) return;
 
   // send message to discord
-  const discordWebhookUrl = Deno.env.get('DISCORD_WEBHOOKS_URL');
+  const discordWebhookUrl = Deno.env.get("DISCORD_WEBHOOKS_URL");
   const params = {
-    username: 'GFI-Catsup [beta]',
-    avatar_url: 'https://github.com/open-sauced/assets/blob/master/logo.png?raw=true',
+    username: "GFI-Catsup [beta]",
+    avatar_url: "https://github.com/open-sauced/assets/blob/master/logo.png?raw=true",
     content: `New good first issue: ${context.payload.issue.html_url}`,
   };
 
   // send post request using fetch to webhook
   await fetch(discordWebhookUrl, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(params),
   });
 });
 
 export const config = {
-  path: '/api/github/webhooks',
+  path: "/api/github/webhooks",
 };
 
 /**
@@ -66,21 +71,21 @@ export const config = {
 export default async (request) => {
   try {
     await app.webhooks.verifyAndReceive({
-      id: request.headers.get('x-github-delivery'),
-      name: request.headers.get('x-github-event'),
-      signature: request.headers.get('x-hub-signature-256').replace(/sha256=/, ''),
+      id: request.headers.get("x-github-delivery"),
+      name: request.headers.get("x-github-event"),
+      signature: request.headers.get("x-hub-signature-256").replace(/sha256=/, ""),
       payload: await request.text(),
     });
 
     return new Response('{ "ok": true }', {
-      headers: { 'content-type': 'application/json' },
+      headers: { "content-type": "application/json" },
     });
   } catch (error) {
     app.log.error(error);
 
     return new Response(`{ "error": "${error.message}" }`, {
       status: 500,
-      headers: { 'content-type': 'application/json' },
+      headers: { "content-type": "application/json" },
     });
   }
 };
