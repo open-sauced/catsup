d98d245a8c13adbac2b32a51c7217f629dd0f844
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2023-07-30 20:59:01 -0700

    add debug log

--- github-app.js
+++ github-app.js
@@ -5,7 +5,7 @@ const GOOD_FIRST_REGEX = /^good\sfirst\sissue$/i;
  * @param {import("@octokit/app").App} app
  */
 export default async function githubApp(env, app) {
-  app.log.info("App loaded");
+  app.log.info('App loaded');
 
   app.webhooks.onAny(async ({ name, payload }) => {
     const eventNameWithAction = payload.action ? `${name}.${payload.action}` : name;
@@ -13,7 +13,7 @@ export default async function githubApp(env, app) {
     app.log.info(`Event received: ${eventNameWithAction}`);
   });
 
-  app.webhooks.on("issues.labeled", async (context) => {
+  app.webhooks.on('issues.labeled', async (context) => {
     const { name } = context.payload.label;
 
     if (!GOOD_FIRST_REGEX.test(name)) return;
@@ -21,16 +21,16 @@ export default async function githubApp(env, app) {
     // send message to discord
     const discordWebhookUrl = env.DISCORD_WEBHOOKS_URL;
     const params = {
-      username: "GFI-Catsup [beta]",
-      avatar_url: "https://github.com/open-sauced/assets/blob/master/logo.png?raw=true",
+      username: 'GFI-Catsup [beta]',
+      avatar_url: 'https://github.com/open-sauced/assets/blob/master/logo.png?raw=true',
       content: `New good first issue: ${context.payload.issue.html_url}`,
     };
 
     // send post request using fetch to webhook
     await fetch(discordWebhookUrl, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(params),
     });
